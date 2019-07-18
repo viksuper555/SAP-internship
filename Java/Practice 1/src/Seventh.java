de0e49f1885e982113af22519368f4f8f68ef5e8
Viktor Kolev <viksuper555@gmail.com>
2019-07-18 10:54:40 +0300

    Updated Tasks 7 and 11

--- Java/Practice 1/src/Seventh.java
+++ Java/Practice 1/src/Seventh.java
@@ -2,12 +2,16 @@
 public class Seventh {
 	public static void main(String args[]){		//Find multiplication of nums
 												// below the main diagonal
-	int arr[][] = {{1,2,3,4}, {5,6,7,8}, {1,3,5,7}}, multi = 1;
+	int arr[][] = {{1,2,3}, {5,6,7}, {2,3,5}}, multi = 1;
 	
-	int len = Math.min(arr.length, arr[0].length);
+	int len = arr.length;
 	for(int i = 1; i < len; i++)
 	{
-		multi *= arr[i][i-1];
+		for(int j = 0; j < i; j++)
+		{
+			multi *= arr[i][j];
+		}
+		
 	}
 	System.out.print(multi);
 	}
--- Java/Practice 1/src/TaskEleven.java
+++ Java/Practice 1/src/TaskEleven.java
@@ -5,20 +5,24 @@ public class TaskEleven {			//Create a matrix from user input
 	{
 		int n, m;
 		Scanner scanner = new Scanner(System.in);
-		
+		do {	
 		System.out.println("Enter N:");
 		n = scanner.nextInt();
+		}while(n < 0);
 		
+		do {
 		System.out.println("Enter M:");
 		m = scanner.nextInt();
-		
+		}while(n < 0);
 		int arr[][] = new int[n][m];
 		
 		for(int i = 0; i < n; i ++)
 		{
 			for(int j = 0; j < m; j++)
 			{
+				do {
 				arr[i][j] = scanner.nextInt();
+				}while(arr[i][j] < Integer.MIN_VALUE || arr[i][j] > Integer.MAX_VALUE);
 			}
 		}
 		
